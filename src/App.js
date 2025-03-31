97f3c9285f5c95aa7c49f3ef130016e9d3c4358d
Mayank Mehta <mayankmehta880@gmail.com>
2025-03-31 15:21:05 +0530

    added required images to the folder

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Magic Memory..</h1>
+      <h1>Magic Memory</h1>
       <button>New Game</button>
     </div>
   );
