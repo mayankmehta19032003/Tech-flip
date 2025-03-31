67e1a4fd318c8087bc1a564221876f4e3ec19fb7
Mayank Mehta <mayankmehta880@gmail.com>
2025-03-31 20:22:32 +0530

    update state matched for cards

--- src/App.js
+++ src/App.js
@@ -1,68 +1,74 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import SingleCard from './components/SingleCard';
+import { useEffect, useState } from "react";
+import "./App.css";
+import SingleCard from "./components/SingleCard";
 
 const cardImages = [
-  {"src": "/img/helmet-1.png"},
-  {"src": "/img/potion-1.png"},
-  {"src": "/img/ring-1.png"},
-  {"src": "/img/scroll-1.png"},
-  {"src": "/img/shield-1.png"},
-  {"src": "/img/sword-1.png"},
-]
+  { src: "/img/helmet-1.png", matched: false },
+  { src: "/img/potion-1.png", matched: false },
+  { src: "/img/ring-1.png", matched: false },
+  { src: "/img/scroll-1.png", matched: false },
+  { src: "/img/shield-1.png", matched: false },
+  { src: "/img/sword-1.png", matched: false },
+];
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
 
-  const [cards,setCards] = useState([]);
-  const [turns,setTurns] = useState(0);
-
-  const [choiceOne,setChoiceOne] = useState(null);
-  const [choiceTwo,setChoiceTwo] = useState(null);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   //shuffle card
-  const shuffleCards =()=>{
-    const shuffleCards = [...cardImages,...cardImages]
-    .sort(()=> Math.random() - 0.5)     //The .sort() method works by comparing two elements at a time //Negative → Swap positions Positive → Keep the order Zero → No change
-    .map((card)=> ({...card, id: Math.random()})) // it will id to every object and copy its all property for now we have only one which is src.
+  const shuffleCards = () => {
+    const shuffleCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5) //The .sort() method works by comparing two elements at a time //Negative → Swap positions Positive → Keep the order Zero → No change
+      .map((card) => ({ ...card, id: Math.random() })); // it will id to every object and copy its all property.
 
     setCards(shuffleCards);
     setTurns(0);
-  }
+  };
 
   //handle choice
   const handleChoice = (card) => {
-    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
-  }
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
 
   //reset choices & increase turn
-  const resetTurn = ()=>{
+  const resetTurn = () => {
     setChoiceOne(null);
     setChoiceTwo(null);
-    setTurns(prevTurns => prevTurns +1);
-  }
+    setTurns((prevTurns) => prevTurns + 1);
+  };
 
   //compare 2 selected cards
-  useEffect(()=>{
-    if(choiceOne && choiceTwo){
-      if(choiceOne.src === choiceTwo.src){
-        console.log("right");
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
         resetTurn();
-      }
-      else{
-        console.log("wrong");
+      } else {
         resetTurn();
       }
     }
-  },[choiceOne,choiceTwo]);
+  }, [choiceOne, choiceTwo]);
 
+  console.log(cards);
   return (
     <div className="App">
       <h1>Magic Memory</h1>
       <button onClick={shuffleCards}>New Game</button>
 
-      <div className='card-grid'>
-        {cards.map(card => (
-          <SingleCard key={card.id} card={card} handleChoice={handleChoice}/> 
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
         ))}
       </div>
     </div>
