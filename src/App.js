c4ae5c53b76fb9ca9cf1b9ba651ca893a2ddac75
Mayank Mehta <mayankmehta880@gmail.com>
2025-03-31 19:54:23 +0530

    created a func for handle choice

--- src/App.js
+++ src/App.js
@@ -16,6 +16,9 @@ function App() {
   const [cards,setCards] = useState([]);
   const [turns,setTurns] = useState(0);
 
+  const [choiceOne,setChoiceOne] = useState(null);
+  const [choiceTwo,setChoiceTwo] = useState(null);
+
   //shuffle card
   const shuffleCards =()=>{
     const shuffleCards = [...cardImages,...cardImages]
@@ -26,7 +29,11 @@ function App() {
     setTurns(0);
   }
 
-  console.log(cards,turns);
+  //handle choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
   return (
     <div className="App">
       <h1>Magic Memory</h1>
@@ -34,7 +41,7 @@ function App() {
 
       <div className='card-grid'>
         {cards.map(card => (
-          <SingleCard key={card.id} card={card}/> 
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice}/> 
         ))}
       </div>
     </div>
